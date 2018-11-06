c97da547fd5d4b2da66f0dd86f3b23aca6d6e722
newtonkiragu <karanunewton4@gmail.com>
2018-11-06 18:23:58 +0300

    mend

--- linux-install/fullstack.sh
+++ linux-install/fullstack.sh
@@ -34,12 +34,12 @@ ok "PostgreSQL"
 
 # VS Code
 if has_not code; then
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo -y install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get -y install apt-transport-https
-sudo apt-get update
-sudo apt-get -y install code
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo -y install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt-get -y install apt-transport-https
+  sudo apt-get update
+  sudo apt-get -y install code
 fi
 ok "VS Code"
 
