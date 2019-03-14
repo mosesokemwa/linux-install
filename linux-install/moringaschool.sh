feff3513c7027a777a5d9883ee9587cdba89862f
newtonkiragu <karanunewton4@gmail.com>
2019-03-14 15:09:51 +0300

    ignored dependencies for atom

--- linux-install/moringaschool.sh
+++ linux-install/moringaschool.sh
@@ -48,7 +48,7 @@ ok "xclip"
 if has_not atom; then
   sudo add-apt-repository ppa:webupd8team/atom
   sudo apt update
-  sudo apt install atom
+  sudo apt install atom --ignore-depends
 fi
 ok "Atom installed"
 
